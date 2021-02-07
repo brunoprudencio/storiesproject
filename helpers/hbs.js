f21c4ac5ae9e0b0d7f2aaa035e9eed2d99a57d6f
Brubatera <brubatera5@gmail.com>
2021-02-07 14:17:41 -0300

    Adding docker and docker-compose

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -2,7 +2,12 @@ const handlebars = require('handlebars')
 
 module.exports = {
     formatDate: function (dateString) {
-        const sdf = new Intl.DateTimeFormat('en', {day: '2-digit', month: 'short', year: '2-digit'})
+        const sdf = new Intl.DateTimeFormat('en-US', {
+            weekday: 'short',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+        })
         return new handlebars.SafeString(
             sdf.format(new Date(dateString)))
     }
